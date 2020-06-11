920043ac1519650de000e19151db37277c9164f1
Toni Peise <toni.peise@gmx.net>
2020-06-11 19:43:47 +0200

    1.2: Button text

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ canvas.pack()
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-openFile = tk.Button(root, text="Open File", padx=10,
+openFile = tk.Button(root, text="Add Apps", padx=10,
                      pady=5, fg="white", bg="#263D42", command=addApp)
 openFile.pack()
 
